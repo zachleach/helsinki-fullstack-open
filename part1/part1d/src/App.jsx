9c5b1b60de13c268da2795bbc45d6a127e97d87c
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-06-10 16:48:40 -0500

    updating state is asynchronous example

--- part1/part1d/src/App.jsx
+++ part1/part1d/src/App.jsx
@@ -1,22 +1,40 @@
 import { useState } from 'react'
 
 const App = () => {
-	const [clicks, setClick] = useState(0)
+	const [left, setLeft] = useState(0)
+	const [right, setRight] = useState(0)
 	const [allClicks, setAll] = useState([])
+	const [total, setTotal] = useState(0)
 
-	const handle_click = () => {
-		setClick(clicks + 1)
-		setAll(allClicks.concat('C'))
+	const handle_left_click = () => {
+		let updated_left = left + 1
+		setLeft(updated_left)
+		setAll(allClicks.concat('L'))
+		setTotal(updated_left + right)
+	}
+
+	const handle_right_click = () => {
+		let updated_right = right + 1
+		setRight(right + 1)
+		setAll(allClicks.concat('R'))
+		setTotal(left + updated_right)
 	}
 
 	return (
 		<div>
-			{clicks}
-			<button onClick={handle_click}>
+			{left}
+			<button onClick={handle_left_click}>
 				Left
 			</button>
+			<button onClick={handle_right_click}>
+				Right
+			</button>
+			{right}
 			<p>
 				{allClicks.join(' ')}
+			</p>
+			<p>
+				Total: {total}
 			</p>
 		</div>
 	)
