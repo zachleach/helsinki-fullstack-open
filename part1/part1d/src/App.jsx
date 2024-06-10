7320f3ef71670207077b47b0fefad799c65903e0
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-06-10 16:39:42 -0500

    state as array example

--- part1/part1d/src/App.jsx
+++ part1/part1d/src/App.jsx
@@ -1,47 +1,23 @@
 import { useState } from 'react'
 
 const App = () => {
-	/* make state objects */
-	const [ clicks, setClicks ] = useState({
-		left: 0, right: 0
-	})
+	const [clicks, setClick] = useState(0)
+	const [allClicks, setAll] = useState([])
 
-	/*
-	const handle_left_click = () => {
-		const amount = {
-			left: clicks.left + 1,
-			right: clicks.right
-		}
-		setClicks(amount)
-	}
-	*/
-	const handle_left_click = () => {
-		setClicks({ ...clicks, left: clicks.left + 1 })
+	const handle_click = () => {
+		setClick(clicks + 1)
+		setAll(allClicks.concat('C'))
 	}
 
-	/*
-	const handle_right_click = () => {
-		const amount = {
-			left: clicks.left,
-			right: clicks.right + 1
-		}
-		setClicks(amount)
-	}
-	*/
-	const handle_right_click = () => {
-		setClicks({ ...clicks, right: clicks.right + 1 })
-	}
-	
 	return (
 		<div>
-			{clicks.left}
-			<button onClick={handle_left_click}>
+			{clicks}
+			<button onClick={handle_click}>
 				Left
 			</button>
-			<button onClick={handle_right_click}>
-				Right
-			</button>
-			{clicks.right}
+			<p>
+				{allClicks.join(' ')}
+			</p>
 		</div>
 	)
 }
