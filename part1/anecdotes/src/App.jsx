12477f9f165220f34a4e5ad5e55e56ff0b60f923
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-06-12 16:12:37 -0500

    complete part 1d anecdotes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -2,7 +2,6 @@
 import { useState } from 'react'
 
 const App = () => {
-	const [selected, setSelected] = useState(0)
 	const anecdotes = [
 		'If it hurts, do it more often.',
 		'Adding manpower to a late software project makes it later!',
@@ -14,7 +13,9 @@ const App = () => {
 		'The only way to go fast, is to go well.'
 	]
 
+	const [selected, setSelected] = useState(0)
 	const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+	const [maxIndex, setMaxIndex] = useState(0)
 
 	const onNext = () => {
 		const max = anecdotes.length
@@ -22,15 +23,34 @@ const App = () => {
 	}
 
 	const onVote = () => {
-		const upd_arr = [...votes]
-		let upd_vote = votes[selected] + 1
-		upd_arr[selected] = upd_vote
-		setVotes(upd_arr)
+		votes[selected] = votes[selected] + 1
+		const upd_votes = [...votes]
+		setVotes(upd_votes)
+
+		setMaxIndex(getMaxIndex(upd_votes))
 	}
+
+	const getMaxIndex = (arr) => {
+		let max_i = 0
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] > arr[max_i]) {
+				max_i = i
+			}
+		}
+
+		return max_i
+	}
+
+
 
 	return (
 
 		<div>
+			<div>
+				<h1>
+					Anecdote of the day
+				</h1>
+			</div>
 			<div>
 				{anecdotes[selected]}
 			</div>
@@ -45,6 +65,18 @@ const App = () => {
 				<button onClick={onNext}>
 					next anecdote
 				</button>
+			</div>
+
+			<div>
+				<h1>
+					Anecdote with most votes
+				</h1>
+				<div>
+					{anecdotes[maxIndex]}
+				</div>
+				<div>
+					has {votes[maxIndex]} votes
+				</div>
 			</div>
 		</div>
 
