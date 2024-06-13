5912bd631a7e5d9dc886f622dc86de35a2fc9b7e
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-06-12 20:06:42 -0500

    complete 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,57 +1,59 @@
 import { useState } from 'react'
+import Course from './components/Course.jsx'
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-
-  return <Course course={course} />
-}
-
-
-
-const Course = ({ course }) => {
-
-	const getTotal = (parts_arr) => {
-		return parts_arr.reduce((total, part) => part.exercises + total, 0)
-	}
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
 	return (
 		<div>
-			<h1>
-				{course.name}
-			</h1>
-			
-			{course.parts.map(part => (
-					<p key={part.id}>{part.name} {part.exercises}</p>
+			{courses.map(course => (
+				<Course key={course.id} course={course} />
 			))}
-
-			<p>
-				total of {getTotal(course.parts)} exercises
-			</p>
-
 		</div>
 	)
 }
-
-
 
 export default App
--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,5 +1,25 @@
-const Course = (props) => {
-	const {course} = props
+const Course = ({ course }) => {
+
+	const getTotal = (parts_arr) => {
+		return parts_arr.reduce((total, part) => part.exercises + total, 0)
+	}
+
+	return (
+		<div>
+			<h1>
+				{course.name}
+			</h1>
+			
+			{course.parts.map(part => (
+					<p key={part.id}>{part.name} {part.exercises}</p>
+			))}
+
+			<p>
+				total of {getTotal(course.parts)} exercises
+			</p>
+
+		</div>
+	)
 }
 
 export default Course
