0aa601df8487ee94c8e2229cdd2fd4319ae8ca11
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-06-12 21:48:52 -0500

    compelte 2.6 (begin phonebook)

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+
+  const [newName, setNewName] = useState('')
+
+	{/* input monitor */}
+	const handleNameSubmission = (event) => {
+		console.log(event.target.value)
+		setNewName(event.target.value)
+	}
+
+	{/* form submit function */ }
+	const addName = (event) => {
+		event.preventDefault()
+
+		const p = {
+			name: newName
+		}
+
+		setPersons(persons.concat(p))
+		setNewName('')
+	}
+
+  return (
+    <div>
+
+      <h2>
+				Phonebook
+			</h2>
+
+			{/* on form submission, call addName() */}
+      <form onSubmit={addName}>
+        <div>
+					{/* keep track of text within the input box */}
+          name: <input 
+						value={newName}
+						onChange={handleNameSubmission}
+					/>
+        </div>
+
+        <div>
+          <button type="submit">
+						add
+					</button>
+        </div>
+      </form>
+
+      <h2>
+				Numbers
+			</h2>
+
+		{/* display the persons[] state array, each value wrapped in a div */}
+		<ul>
+			{persons.map((person) => 
+				<div key={person.name}>{person.name}</div>
+			)}
+		</ul>
+
+    </div>
+  )
+}
+
+export default App
