1d7c27c34486c63e8726ea019e73d61c91b65c17
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-06-13 12:59:42 -0500

    2024-06-13 12:59:42

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,28 +1,30 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "040-123456", id: 1 },
-    { name: 'ARTt HELL', number: "10293123", id: 2 },
-    { name: 'artt madasd', number: "123-421-32", id: 3 },
-    { name: 'mike', number: "11111", id: 4 },
-  ]) 
+const Filter = ({ state, setFunction }) => {
+	console.log(state)
+	return (
+		<div>
+			filter shown with <input
+				value={state}
+				onChange={(e) => setFunction(e.target.value)}
+			/>
+		</div>
+	)
+}
 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+const Persons = ({ peopleArray, filterInput }) => {
+	const filtered_people = peopleArray.filter(p => p.name.toLowerCase().includes(filterInput.toLowerCase()))
+	return (
+		<div>
+			{filtered_people.map((person) => 
+				<div key={person.name}>{person.name} {person.number}</div>
+			)}
+		</div>
+	)
+}
 
-	{/* input monitors */}
-	const handleNameInputChange = (event) => {
-		console.log(event.target.value)
-		setNewName(event.target.value)
-	}
-	const handleNumberInputChange = (event) => {
-		console.log(event.target.value)
-		setNewNumber(event.target.value)
-	}
-
-
-
+const InputForm = ({ persons, setPersons, newName, setNewName, newNumber, setNewNumber }) => {
+	
 	{/* form submit function */ }
 	const addName = (event) => {
 		event.preventDefault()
@@ -42,54 +44,63 @@ const App = () => {
 		}
 	}
 
+	return (
+		/* on form submission, call addName() */
+		<form onSubmit={addName}>
+			<div>
+				{/* keep track of text within the input box */}
+				name: <input 
+					value={newName}
+					onChange={(e) => setNewName(e.target.value)}
+				/>
+			</div>
+			<div>
+				{/* keep track of text within the input box */}
+				number: <input 
+					value={newNumber}
+					onChange={(e) => setNewNumber(e.target.value)}
+				/>
+			</div>
+
+			<div>
+				<button type="submit">
+					add
+				</button>
+			</div>
+		</form>
+	)
+}
 
 
 
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: "040-123456", id: 1 },
+    { name: 'ARTt HELL', number: "10293123", id: 2 },
+    { name: 'artt madasd', number: "123-421-32", id: 3 },
+    { name: 'mike', number: "11111", id: 4 },
+  ]) 
 
-
-
-
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+	const [filter, setFilter] = useState('')
 
   return (
     <div>
-
       <h2>
 				Phonebook
 			</h2>
+			<Filter state={filter} setFunction={setFilter}/>
 
-			{/* on form submission, call addName() */}
-      <form onSubmit={addName}>
-        <div>
-					{/* keep track of text within the input box */}
-          name: <input 
-						value={newName}
-						onChange={handleNameInputChange}
-					/>
-        </div>
-        <div>
-					{/* keep track of text within the input box */}
-          number: <input 
-						value={newNumber}
-						onChange={handleNumberInputChange}
-					/>
-        </div>
-
-
-        <div>
-          <button type="submit">
-						add
-					</button>
-        </div>
-      </form>
+      <h2>
+				Add a new
+			</h2>
+			<InputForm persons={persons} setPersons={setPersons} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber}/>
 
       <h2>
 				Numbers
 			</h2>
-
-			{/* display the persons[] state array, each value wrapped in a div */}
-			{persons.map((person) => 
-				<div key={person.name}>{person.name} {person.number}</div>
-			)}
+			<Persons peopleArray={persons} filterInput={filter} />
 
     </div>
   )
