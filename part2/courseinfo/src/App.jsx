c189d2c37c85d3d0d01f00274aa7d8689092ec12
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-06-12 20:02:20 -0500

    finish 2.2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -27,7 +27,12 @@ const App = () => {
 }
 
 
+
 const Course = ({ course }) => {
+
+	const getTotal = (parts_arr) => {
+		return parts_arr.reduce((total, part) => part.exercises + total, 0)
+	}
 
 	return (
 		<div>
@@ -35,9 +40,13 @@ const Course = ({ course }) => {
 				{course.name}
 			</h1>
 			
-		{course.parts.map(part => (
-				<p key={part.id}>{part.name} {part.exercises}</p>
-		))}
+			{course.parts.map(part => (
+					<p key={part.id}>{part.name} {part.exercises}</p>
+			))}
+
+			<p>
+				total of {getTotal(course.parts)} exercises
+			</p>
 
 		</div>
 	)
