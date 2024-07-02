5c0727a4ae084ebf3f4afa227b7caebe6b2e4449
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-07-02 01:39:24 -0500

    2024-07-02 01:39:24

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,26 +4,26 @@ const app = express()
 app.use(express.json())
 
 let people = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+	{ 
+		"id": "1",
+		"name": "Arto Hellas", 
+		"number": "040-123456"
+	},
+	{ 
+		"id": "2",
+		"name": "Ada Lovelace", 
+		"number": "39-44-5323523"
+	},
+	{ 
+		"id": "3",
+		"name": "Dan Abramov", 
+		"number": "12-43-234345"
+	},
+	{ 
+		"id": "4",
+		"name": "Mary Poppendieck", 
+		"number": "39-23-6423122"
+	}
 ]
 
 app.get('/api/persons', (request, response) => {
@@ -54,7 +54,10 @@ app.get('/api/persons/:id', (request, response) => {
 	response.json(person)
 })
 
-/* 3.4: add a route for deleting /api/persons/:id */
+/**
+ * 3.4: add a route for deleting /api/persons/:id 
+ *
+ */
 app.delete('/api/persons/:id', (request, response) => {
 	const id = request.params.id
 	people = people.filter(person => person.id !== id)
@@ -63,20 +66,37 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 
-/* 3.5: add a route for post requests to /api/persons */
+/**
+ * 3.5: add a route for post requests to /api/persons 
+ *
+ */
 app.post('/api/persons', (request, response) => {
 	const new_id = String(Math.ceil(Math.random() * 100000000000))
-	console.log(request.body)
+	const body = request.body
+
+	/* 3.6: the request is not allowed to succeed if name or number is missing */
+	if (body.name === undefined) {
+		return response.status(400).json({ error: "name is missing" })
+	}
+
+	/* 3.6: the request is not allowed to succeed if name or number is missing */
+	if (body.number === undefined) {
+		return response.status(400).json({ error: "number is missing" })
+	}
 
 	const new_person = {
 		id: new_id,
-		name: request.body.name,
-		number: request.body.number
+		name: body.name,
+		number: body.number
 	}
 
 	people = people.concat(new_person)
 	response.json(people)
 })
+
+
+
+
 
 
 
