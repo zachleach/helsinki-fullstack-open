bbf3bb8bcb6468d124581024306252a21d103a7c
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-07-04 07:31:13 -0500

    2024-07-04 07:31:13

--- part3/notes/backend/index.js
+++ part3/notes/backend/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
+
 
 app.use(express.json())
 
@@ -75,7 +78,7 @@ app.post('/api/notes', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
