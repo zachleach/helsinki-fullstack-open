f9e68b5dbc618433432c0f152a0b1f3aed111c36
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-08-03 15:23:01 -0500

    2024-08-03 15:23:01

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react'
+
+/* 1.12* initialize project */
 const App = () => {
-	return (
-		<>
-			Hello World!
-		</>
-	)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  return (
+    <div>
+			<div>
+				{anecdotes[selected]}
+			</div>
+
+			{/* 1.12* add a button that can be clicked to generate a random anecdote */}
+			<button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>
+				next anecdote
+			</button>
+    </div>
+  )
 }
 
 export default App
