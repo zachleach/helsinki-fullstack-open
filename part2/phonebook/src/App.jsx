8b085d514ae62146c23cfb81288f3961f5a4ff8f
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-08-04 18:20:14 -0500

    finish 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,28 +2,35 @@ import { useState } from 'react'
 
 /* 2.6 implement functionality for adding items to the phonebook */
 const Form = ({ phonebook, set_phonebook }) => {
-  const [new_name, set_new_name] = useState('')
+  const [input_field, set_input_field] = useState('')
 
 	const handle_submit = (event) => {
 		event.preventDefault()
-		const new_person = {
-			name: new_name
+		/* 2.7 alert and prevent users from inputting names already in the phonebook */
+		if (phonebook.find(obj => obj.name === input_field)) {
+			window.alert(`${input_field} already in phonebook!`)
+			return
 		}
 
+		const new_person = {
+			name: input_field
+		}
+
+
 		set_phonebook(phonebook.concat(new_person))
-		set_new_name('')
+		set_input_field('')
 	}
 
 	const handle_input_change = (event) => {
 		console.log(event.target.value)
-		set_new_name(event.target.value)
+		set_input_field(event.target.value)
 	}
 
 	return (
 		<form onSubmit={handle_submit}>
 			<div>
 				name: 
-				<input value={new_name} onChange={handle_input_change}/>
+				<input value={input_field} onChange={handle_input_change}/>
 			</div>
 			<div>
 				<button type="submit">
