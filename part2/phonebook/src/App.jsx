f13365f90edc1a8fbe69a42644b972b8364353c6
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-08-04 18:46:35 -0500

    add 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,35 +2,50 @@ import { useState } from 'react'
 
 /* 2.6 implement functionality for adding items to the phonebook */
 const Form = ({ phonebook, set_phonebook }) => {
-  const [input_field, set_input_field] = useState('')
+  const [name_input_field, set_name_input_field] = useState('')
+
+	{/* 2.8 expand application to allow users to add phone numbers */}
+  const [number_input_field, set_number_input_field] = useState('')
 
 	const handle_submit = (event) => {
 		event.preventDefault()
+
 		/* 2.7 alert and prevent users from inputting names already in the phonebook */
-		if (phonebook.find(obj => obj.name === input_field)) {
-			window.alert(`${input_field} already in phonebook!`)
+		if (phonebook.find(obj => obj.name === name_input_field)) {
+			window.alert(`${name_input_field} already in phonebook!`)
 			return
 		}
 
 		const new_person = {
-			name: input_field
+			name: name_input_field,
+			number: number_input_field
 		}
 
-
 		set_phonebook(phonebook.concat(new_person))
-		set_input_field('')
+		set_name_input_field('')
+		set_number_input_field('')
 	}
 
-	const handle_input_change = (event) => {
+	const handle_name_input_change = (event) => {
 		console.log(event.target.value)
-		set_input_field(event.target.value)
+		set_name_input_field(event.target.value)
+	}
+
+	const handle_number_input_change = (event) => {
+		console.log(event.target.value)
+		set_number_input_field(event.target.value)
 	}
 
 	return (
 		<form onSubmit={handle_submit}>
 			<div>
 				name: 
-				<input value={input_field} onChange={handle_input_change}/>
+				<input value={name_input_field} onChange={handle_name_input_change}/>
+			</div>
+			{/* 2.8 expand application to allow users to add phone numbers */}
+			<div>
+				number: 
+				<input value={number_input_field} onChange={handle_number_input_change}/>
 			</div>
 			<div>
 				<button type="submit">
@@ -44,7 +59,8 @@ const Form = ({ phonebook, set_phonebook }) => {
 const App = () => {
   const [phonebook, set_phonebook] = useState([
     { 
-			name: 'Arto Hellas'
+			name: 'Arto Hellas',
+			number: '1'
 		}
   ]) 
 
@@ -64,7 +80,7 @@ const App = () => {
 			<div>
 				{phonebook.map(item => (
 					<div key={item.name}>
-						{item.name}
+						{item.name} {item.number}
 					</div>
 				))}
 			</div>
