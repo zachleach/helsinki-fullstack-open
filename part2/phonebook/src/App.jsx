49c739ac0c92b6b0eabe85ce949caa54a00e9938
zachleach@wsl <85465371+zachleach@users.noreply.github.com>
2024-08-04 19:32:44 -0500

    2024-08-04 19:32:44

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,52 +1,5 @@
 import { useState } from 'react'
 
-/* 2.6 implement functionality for adding items to the phonebook */
-const Form = ({ phonebook, set_phonebook }) => {
-  const [name_input_field, set_name_input_field] = useState('')
-
-	{/* 2.8 expand application to allow users to add phone numbers */}
-  const [number_input_field, set_number_input_field] = useState('')
-
-	const handle_submit = (event) => {
-		event.preventDefault()
-
-		/* 2.7 alert and prevent users from inputting names already in the phonebook */
-		if (phonebook.find(obj => obj.name === name_input_field)) {
-			window.alert(`${name_input_field} already in phonebook!`)
-			return
-		}
-
-		const new_person = {
-			name: name_input_field,
-			number: number_input_field
-		}
-
-		set_phonebook(phonebook.concat(new_person))
-		set_name_input_field('')
-		set_number_input_field('')
-	}
-
-	return (
-		<form onSubmit={handle_submit}>
-			<div>
-				name: 
-				<InputState state={name_input_field} set_state={set_name_input_field} />
-			</div>
-			{/* 2.8 expand application to allow users to add phone numbers */}
-			<div>
-				number: 
-				<InputState state={number_input_field} set_state={set_number_input_field} />
-			</div>
-			<div>
-				<button type="submit">
-					add
-				</button>
-			</div>
-		</form>
-	)
-}
-
-
 /* 2.10 extract three components from the application */
 const InputState = ({ state, set_state }) => {
 	const handle_input_change = (event) => {
@@ -66,6 +19,33 @@ const App = () => {
 			number: '1'
 		}
   ]) 
+
+	/* 2.8 expand application to allow users to add phone numbers */
+  const [number_input_field, set_number_input_field] = useState('')
+
+	/* 2.6 implement functionality for adding items to the phonebook */
+  const [name_input_field, set_name_input_field] = useState('')
+	const handle_submit = (event) => {
+		event.preventDefault()
+
+		/* 2.7 alert and prevent users from inputting names already in the phonebook */
+		if (phonebook.find(obj => obj.name === name_input_field)) {
+			window.alert(`${name_input_field} already in phonebook!`)
+			return
+		}
+
+		const new_person = {
+			name: name_input_field,
+			/* 2.8 expand application to allow users to add phone numbers */
+			number: number_input_field
+		}
+
+		set_phonebook(phonebook.concat(new_person))
+		set_name_input_field('')
+		/* 2.8 expand application to allow users to add phone numbers */
+		set_number_input_field('')
+	}
+
 
 	/* 2.9* implement a search field that can be used to filter the list of people displayed by name */
 	const [filter_input_field, set_filter_input_field] = useState('')
@@ -93,7 +73,22 @@ const App = () => {
 			</h2>
 
 			{/* 2.6 implement functionality for adding items to the phonebook */}
-			<Form {...{ phonebook, set_phonebook }}/>
+			<form onSubmit={handle_submit}>
+				<div>
+					name: 
+					<InputState state={name_input_field} set_state={set_name_input_field} />
+				</div>
+				{/* 2.8 expand application to allow users to add phone numbers */}
+				<div>
+					number: 
+					<InputState state={number_input_field} set_state={set_number_input_field} />
+				</div>
+				<div>
+					<button type="submit">
+						add
+					</button>
+				</div>
+			</form>
 
       <h2>
 				Numbers
